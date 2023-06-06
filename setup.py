e4954e21a776e88f6639a5fa74b93618f2b213d3
allexa-remigio <65001005+allexa-remigio@users.noreply.github.com>
2023-06-06 01:10:07 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='bwdtools',
+    version='1.0.0',
+    description='CSE185 Group 27 Project: BWDTools',
+    author='Archishma Kavalipati, Allexa Remigio, Iris Lee',
+    author_email='',
+    url='https://github.com/archishma/CSE185-Group27',
+    packages=['bwdtools_package']
+)
